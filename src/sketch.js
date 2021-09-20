b6f55362373f74d5379d38efa595dcf8c5382589
liuceyi <602483575@qq.com>
2021-09-20 11:30:59 +0800

    Init

---
+++ src/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(0, 100, 200);
+
+  // background(230);
+  // textAlign(CENTER);
+  // textSize(40);
+  // fill(color('#919191'));
+  // text("MihoSaku", width/2, height/2);
+}
